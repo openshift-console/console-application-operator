f9d6d06f62926cda07ca037f6c5c7e6044811c17
Francisc Munteanu <fmuntean@redhat.com>
2024-08-13 15:39:07 +0200

    remove duplicated logs and requeue with error

--- controller/result.go
+++ controller/result.go
@@ -14,11 +14,6 @@ func RequeueOnError(err error) (ctrl.Result, error) {
 	return ctrl.Result{}, err
 }
 
-// RequeueWithError triggers a object requeue because the informed error happened.
-func RequeueWithError(err error) (ctrl.Result, error) {
-	return ctrl.Result{Requeue: true}, err
-}
-
 // NoRequeue all done, the object does not need reconciliation anymore.
 func NoRequeue() (ctrl.Result, error) {
 	return ctrl.Result{Requeue: false}, nil
